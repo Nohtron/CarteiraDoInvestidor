165b3bc9717a5f8268860f5025e32c83d5445ddd
Northon T. Cavazzoni <northoncavazzoni@hotmail.com>
2020-09-08 14:26:34 -0300

    Criacao inicial do projeto

---
+++ CarteiraConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CarteiraConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
